0b794642c9065a4568f4897be862700d26ef4d32
jaenudin0901 <jaenudinj913198@gmail.com>
2019-09-18 23:51:39 +0700

    menambahkan file ddl.php

---
+++ ddl.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DATA DEFINITION LANGUAGE</title>
+</head>
+<body>
+	<H1>1. Create</H1>
+	<h1>2. Alter</h1>
+	<h1>3. Rename</h1>
+	<h1>4. Drop</h1>
+
+</body>
+</html>
